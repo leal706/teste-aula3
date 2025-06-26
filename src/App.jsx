fadf1e804a478ca649465fe8dbcf11b874faa2a3
VITOR LEAL MARQUES <0151125>
2025-06-26 20:16:06 -0300

    tropa da upa

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>salve tropinha do upa</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
