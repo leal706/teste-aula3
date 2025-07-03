55c6c433b88f94aa8d43c165a57328ebfb4fd188
VITOR LEAL MARQUES <0151125>
2025-07-03 20:57:53 -0300

    att 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-export default function App() {
-  return (
-    <div className="bg-pink-300 min-h-screen flex flex-col">
-      
-      {/* Barra branca no topo */}
-      <header className="bg-white py-4 px-6 flex items-center justify-center">
-        <h1 className="text-2xl font-bold text-purple-800 flex items-center gap-2">
-          <span className="select-none">=^.^=</span>
-          DudaSys
-          <span className="select-none">=^.^=</span>
-        </h1>
-      </header>
+import './App.css'
+import { FaCat } from "react-icons/fa6";
 
-      {/* Conteúdo central */}
-      <div className="flex flex-1 items-center justify-center">
+
+
+function App() {
+   return <div className="bg-pink-300 min-h-screen flex flex-col">
+  
+  <header className="bg-white py-4 px-6 flex items-center justify-center"> 
+  <h1 className='text-2xl font-bold text-purple-800 flex items-center'><FaCat />DudaSys<FaCat /> </h1> 
+  </header>
+
+      
+  <div className="flex flex-1 items-center justify-center">
         <form
-          action="#"
+          action="https://getform.io/f/azynewlb"
           method="POST"
           className="bg-white p-8 rounded-lg shadow-lg w-80"
         >
           <h2 className="text-2xl font-bold mb-6 text-gray-700 text-center">
             Entrar na sua conta
           </h2>
-          <form action="https://getform.io/f/VITOR LEAL MARQUES" method="POST"></form>
           <input
             type="email"
             name="email"
@@ -46,5 +44,19 @@ export default function App() {
       </div>
       
     </div>
-  );
 }
+
+
+
+  
+
+
+
+  
+
+
+
+
+   
+  
+export default App
